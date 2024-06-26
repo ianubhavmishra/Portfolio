b13c9654c268bbb0dc3ff95ac7b40ddff1056e3e
[ianubhavmishra] <[anubhavmishra2408@gmail.com]>
2024-06-26 13:02:54 +0530

    commit-2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,10 @@ const navbar = () => {
     <nav className='flex justify-around items-center p-4 w-full fixed top-0 bg-amber-400 max-md:flex-col max-md:gap-2 max-md:p-2'>
         <h1 className='font-semibold text-4xl'>Frontend Devloper</h1>
       <ul className='flex gap-5'>
-      <li className='hover:underline cursor-pointer'><a href="#about">About</a></li>
-      <li className='hover:underline cursor-pointer'><a href="#skills">Skills</a></li>
-      <li className='hover:underline cursor-pointer'><a href="#education">Education</a></li>
-      <li className='hover:underline cursor-pointer'><a href="#contact">Contact</a></li>
+      <li className='hover:underline'><a href="#about">About</a></li>
+      <li className='hover:underline'><a href="#skills">Skills</a></li>
+      <li className='hover:underline'><a href="#education">Education</a></li>
+      <li className='hover:underline'><a href="#contact">Contact</a></li>
       </ul>
     </nav>
   )
